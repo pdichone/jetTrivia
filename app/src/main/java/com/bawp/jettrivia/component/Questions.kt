4f2a688b45d4a10aea54635d1fbdc5ef089202db
pdichone <pdichone@gmail.com>
2021-11-02 09:02:56 -0700

    Finished with app logic

--- app/src/main/java/com/bawp/jettrivia/component/Questions.kt
+++ app/src/main/java/com/bawp/jettrivia/component/Questions.kt
@@ -6,10 +6,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +20,7 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -106,7 +105,10 @@ fun QuestionDisplay(
         Column(modifier = Modifier.padding(12.dp),
               verticalArrangement = Arrangement.Top,
               horizontalAlignment = Alignment.Start) {
-            QuestionTracker(counter = questionIndex.value)
+
+            if (questionIndex.value >= 3) ShowProgress(score = questionIndex.value)
+
+            QuestionTracker(counter = questionIndex.value, viewModel.getTotalQuestionCount())
             DrawDottedLine( pathEffect )
 
             Column {
@@ -203,7 +205,57 @@ fun DrawDottedLine(pathEffect: PathEffect) {
 
 }
 
+@Preview
+@Composable
+fun ShowProgress(score: Int = 12) {
 
+    val gradient = Brush.linearGradient(listOf(Color(0xFFF95075),
+                                              Color(0xFFBE6BE5)))
+
+     val progressFactor by remember(score) {
+         mutableStateOf(score*0.005f)
+
+     }
+    Row(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(45.dp)
+        .border(width = 4.dp,
+            brush = Brush.linearGradient(colors = listOf(AppColors.mLightPurple,
+                AppColors.mLightPurple)),
+            shape = RoundedCornerShape(34.dp))
+        .clip(RoundedCornerShape(topStartPercent = 50,
+            topEndPercent = 50,
+            bottomEndPercent = 50,
+            bottomStartPercent = 50))
+        .background(Color.Transparent),
+       verticalAlignment = Alignment.CenterVertically) {
+        Button(
+            contentPadding = PaddingValues(1.dp),
+            onClick = { },
+              modifier = Modifier
+                  .fillMaxWidth(progressFactor)
+                  .background(brush = gradient),
+              enabled = false,
+              elevation = null,
+              colors = buttonColors(
+                  backgroundColor = Color.Transparent,
+                  disabledBackgroundColor = Color.Transparent
+                                   )) {
+               Text(text = (score*10).toString(),
+                   modifier = Modifier.clip(shape = RoundedCornerShape(23.dp))
+                       .fillMaxHeight(0.87f)
+                       .fillMaxWidth()
+                       .padding(6.dp),
+                   color = AppColors.mOffWhite,
+                   textAlign = TextAlign.Center)
+
+//
+        }
+
+
+    }
+}
 
 
 
--- app/src/main/java/com/bawp/jettrivia/screens/QuestionsViewModel.kt
+++ app/src/main/java/com/bawp/jettrivia/screens/QuestionsViewModel.kt
@@ -32,6 +32,9 @@ class QuestionsViewModel @Inject constructor( private val repository: QuestionRe
         }
 
     }
+    fun getTotalQuestionCount(): Int {
+        return data.value.data?.toMutableList()?.size!!
+    }
 
 
 
