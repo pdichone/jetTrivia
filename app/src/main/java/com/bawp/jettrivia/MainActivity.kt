d8ff97c92c2de60f8447596dc48a6c20b496234c
pdichone <pdichone@gmail.com>
2021-11-16 11:18:42 -0800

    Update MainActivity.kt

--- app/src/main/java/com/bawp/jettrivia/MainActivity.kt
+++ app/src/main/java/com/bawp/jettrivia/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.bawp.jettrivia
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.bawp.jettrivia.screens.QuestionsViewModel
 import com.bawp.jettrivia.screens.TriviaHome
 import com.bawp.jettrivia.ui.theme.JetTriviaTheme
 import dagger.hilt.android.AndroidEntryPoint
